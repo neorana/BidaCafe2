94443c49bcfe82ec64aa9a407b14d3d3d9c76538
Neo <Neo@DESKTOP-PJ7I3TS>
2022-01-17 15:44:40 +0800

    Add project files.

---
+++ BidaCafe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BidaCafe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellos World!");
+        }
+    }
+}
